ad4633d2781472701f7e80885d8c84f132ce740c
Seth Todd <seth.todd@outlook.com>
2020-03-15 23:46:18 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  
+  attr_accessor :name
+  
+  attr_reader :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    Project.new(project)
+    project.backers << self
+  end
+    
+end
